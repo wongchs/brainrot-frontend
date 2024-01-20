457abb32ea5282b7576ae341ecf26c7a90695f46
Chyisiang <chyisiang8@gmail.com>
2024-01-20 17:05:31 +0800

    update: define props

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,19 @@
-const Post = ({ post }) => {
+import { UserInterface } from "../../types";
+
+interface Props {
+  post:
+    | {
+        content: string;
+        user: UserInterface;
+      }
+    | null
+    | undefined;
+}
+
+const Post = ({ post }: Props) => {
+  if (!post) {
+    return <div>loading</div>;
+  }
   return (
     <div>
       <h2>{post.content}</h2>
