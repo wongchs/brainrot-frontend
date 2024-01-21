208403113ea44f4eb11efb60e981c692a8fb6f1a
Chyisiang <chyisiang8@gmail.com>
2024-01-21 21:55:40 +0800

    update: use shadcn's tabs for edit user details

--- src/components/EditProfile.tsx
+++ src/components/EditProfile.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { UserInterface, UserFormValue } from "types";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 interface props {
   user: UserInterface;
@@ -24,35 +29,54 @@ const EditProfile = ({ user, updateUser }: props) => {
   };
 
   return (
-    <form onSubmit={handleUpdate}>
-      <div>
-        Username:{" "}
-        <input
-          type="text"
-          placeholder={user.username}
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        Name:{" "}
-        <input
-          type="text"
-          placeholder={user.name}
-          value={name}
-          onChange={({ target }) => setName(target.value)}
-        />
-      </div>
-      <div>
-        Password:{" "}
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">Update</button>
-    </form>
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" value={name} onChange={({ target }) => setName(target.value)} />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" value={username} onChange={({ target }) => setUsername(target.value)} />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button onClick={handleUpdate}>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="password">New password</Label>
+              <Input id="password" type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button onClick={handleUpdate}>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
   );
 };
 
