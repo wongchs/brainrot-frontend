e83276356764328978aa1c80a070bbce0081daad
Chyisiang <chyisiang8@gmail.com>
2024-01-24 20:55:41 +0800

    update: improve sidebar styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,8 +15,9 @@ const Sidebar = ({ user, handleLogout, notifications }: props) => {
   return (
     <div className="flex fixed top-0 left-0 w-60 flex-col h-screen p-8 dark:bg-gray-800 shadow duration-100 border-r dark:border-gray-700">
       <div className="space-y-3">
-        <div className="flex items-center justify-between">
-          <h2 className="text-xl font-bold">BRAINROT</h2>
+        <div className="flex items-center justify-between gap-1">
+          <h2 className="text-xl font-bold">💩BRAINROT</h2>
+          <ModeToggle />
         </div>
         <div className="relative">
           <span className="absolute inset-y-0 left-0 flex items-center py-4">
@@ -31,9 +32,9 @@ const Sidebar = ({ user, handleLogout, notifications }: props) => {
             className="w-full py-2 pl-10 text-sm rounded-md focus:outline-none"
           />
         </div>
-        <div className="flex flex-col w- h-full gap-56">
+        <div className="flex flex-col w- h-full gap-64">
           <ul className="pt-2 pb-4 space-y-1 text-sm flex-grow">
-            <li className="rounded-sm">
+            <li className="rounded-sm hover:bg-gray-200 dark:hover:bg-slate-600">
               <Link
                 to="/"
                 className="flex items-center p-2 space-x-3 rounded-md"
@@ -42,7 +43,7 @@ const Sidebar = ({ user, handleLogout, notifications }: props) => {
                 <span>Home</span>
               </Link>
             </li>
-            <li className="rounded-sm">
+            <li className="rounded-sm hover:bg-gray-200 dark:hover:bg-slate-600">
               <Link
                 to={`/profile/${user.id}/${user.username}`}
                 className="flex items-center p-2 space-x-3 rounded-md"
@@ -51,13 +52,7 @@ const Sidebar = ({ user, handleLogout, notifications }: props) => {
                 <span>Profile</span>
               </Link>
             </li>
-
-            <li className="rounded-sm">
-              <div className="flex items-center py-2 space-x-3 rounded-md">
-                <ModeToggle />
-              </div>
-            </li>
-            <li className="rounded-sm">
+            <li className="rounded-sm hover:bg-gray-200 dark:hover:bg-slate-600">
               <Popover>
                 <PopoverTrigger className="flex items-center p-2 space-x-3 rounded-md">
                   <div className="relative">
@@ -69,9 +64,13 @@ const Sidebar = ({ user, handleLogout, notifications }: props) => {
                   <span>Notifications</span>
                 </PopoverTrigger>
                 <PopoverContent>
-                  {notifications.map((notification, index) => (
-                    <p key={index}>{notification.message}</p>
-                  ))}
+                  {notifications.length > 0 ? (
+                    notifications.map((notification, index) => (
+                      <p key={index}>{notification.message}</p>
+                    ))
+                  ) : (
+                    <p>No notifications</p>
+                  )}
                 </PopoverContent>
               </Popover>
             </li>
@@ -79,13 +78,15 @@ const Sidebar = ({ user, handleLogout, notifications }: props) => {
           <ul className="pt-2 pb-4 space-y-1 text-sm">
             {user && (
               <li className="rounded-sm">
-                <div className="flex items-center py-2 space-x-3 rounded-md">
-                  <p className="text-sm">Logged in as: {user.username}</p>
+                <div className="flex items-center p-2 space-x-3 rounded-md">
+                  <p className="text-sm">
+                    Logged in as: <span className="font-bold underline">{user.username}</span>
+                  </p>
                 </div>
               </li>
             )}
             {user && (
-              <li className="rounded-sm">
+              <li className="rounded-sm hover:bg-gray-200 dark:hover:bg-slate-600">
                 <button
                   onClick={handleLogout}
                   className="flex items-center p-2 space-x-3 rounded-md"
